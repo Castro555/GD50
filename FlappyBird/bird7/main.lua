e97ccb1c567c1ad8c56cbabb1ca25db5cee0e7e5
Helcilino Lima <cilinohs@gmail.com>
2022-11-20 16:17:42 -0100

    The Collision Update

--- FlappyBird/bird7/main.lua
+++ FlappyBird/bird7/main.lua
@@ -32,6 +32,8 @@ local spawnTimer = 0
 
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
+local scrolling = true
+
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -68,32 +70,43 @@ function love.keyboard.wasPrssed(key)
 end
 
 function love.update(dt)
-	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-	groundScroll = (groundScroll + GROUND_SROLL_SPEED * dt) % VIRTUAL_WIDTH
-	
-	spawnTimer = spawnTimer + dt
-
-	if spawnTimer > 2 then
-		local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+	if scrolling then
+		backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+		groundScroll = (groundScroll + GROUND_SROLL_SPEED * dt) % VIRTUAL_WIDTH
 		
-		lastY = y
-		
-		table.insert(pipePairs, PipePair(y))
-		spawnTimer = 0
-	end
+		spawnTimer = spawnTimer + dt
 
-	bird:update(dt)
+		if spawnTimer > 2 then
+			local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+			
+			lastY = y
+			
+			table.insert(pipePairs, PipePair(y))
+			spawnTimer = 0
+		end
 
-	for k, pair in pairs(pipePairs) do
-		pair:update(dt)
-	end
+		bird:update(dt)
 
-	for k, pair in pairs(pipePairs) do
-		if pair.remove then
-			table.remove(pipePairs, k)
+		for k, pair in pairs(pipePairs) do
+			pair:update(dt)
+
+			for l, pipe in pairs(pair.pipes) do
+				if bird:collides(pipe) then
+					scrolling = false
+				end
+			end
+
+			if pair.x < -PIPE_WIDTH then
+				pair.remove = true
+			end
+		end
+
+		for k, pair in pairs(pipePairs) do
+			if pair.remove then
+				table.remove(pipePairs, k)
+			end
 		end
 	end
-
 	love.keyboard.keysPressed = {}
 end
 
