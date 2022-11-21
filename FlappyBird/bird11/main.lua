c58873bcb9e60d7cc5e4c0e5ed33e58b9fa98d3c
Castro555 <cilinohs@gmail.com>
2022-11-21 15:51:53 -0100

    The Audio Update

---
+++ FlappyBird/bird11/main.lua
@@ -0,0 +1,118 @@
+push = require 'push'
+Class = require 'class'
+
+require 'Bird'
+
+require 'Pipe'
+
+require 'PipePair'
+
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/CountdownState'
+require 'states/PlayState'
+require 'states/ScoreState'
+require 'states/TitleScreenState'
+
+WINDOW_WIDTH = 1080
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local scrolling = true
+
+function love.load()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	love.window.setTitle('Flappy Bird')
+
+	smallFont = love.graphics.newFont('font.ttf', 8)
+	mediumFont = love.graphics.newFont('flappy.ttf', 14)
+	flappyFont = love.graphics.newFont('flappy.ttf', 28)
+	hugeFont = love.graphics.newFont('flappy.ttf', 56)
+	love.graphics.setFont(flappyFont)
+
+	sounds = {
+		['jump'] = love.audio.newSource('jump.wav', 'static'),
+		['explosion'] = love.audio.newSource('explosion.wav', 'static'),
+		['hurt'] = love.audio.newSource('hurt.wav', 'static'),
+		['score'] = love.audio.newSource('score.wav', 'static'),
+
+		-- https://freesound.org/people/xsgianni/sounds/388079/
+		['music'] = love.audio.newSource('marios_way.mp3', 'static')
+	}
+
+	-- kick off music
+	sounds['music']:setLooping(true)
+	sounds['music']:play()
+
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		vsync = true,
+		fullscreen = false,
+		resizable = true
+	})
+
+	-- initialize state machine with all state-returning functions
+	gStateMachine = StateMachine {
+		['title'] = function() return TitleScreenState() end,
+		['countdown'] = function() return CountdownState() end,
+		['play'] = function() return PlayState() end,
+		['score'] = function() return ScoreState() end
+	}
+	gStateMachine:change('title')
+
+	love.keyboard.keysPressed = {}
+end
+
+
+function love.resize(w, h)
+	push:resize(w, h)
+end
+
+function love.keypressed(key)
+	love.keyboard.keysPressed[key] = true
+
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+function love.keyboard.wasPressed(key)
+	if love.keyboard.keysPressed[key] then
+		return true
+	else
+		return false
+	end
+end
+
+function love.update(dt)
+
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SROLL_SPEED * dt) % VIRTUAL_WIDTH
+	
+	gStateMachine:update(dt)
+
+	love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+	push:start()
+
+	love.graphics.draw(background, -backgroundScroll, 0)
+	gStateMachine:render()
+	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+	push:finish()
+end
